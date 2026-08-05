32d4c61b97c48f4e7bbc1e6294ca54ce146b3ae2
dsjfjkdsfjkdshjkhdsjkashraf <babaashraf062@gmail.com>
2026-08-06 02:08:21 +0500

    Search bar and Categories

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,6 +8,9 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
+  final List<String> categories = ["All", "Mountain", "Beach", "Camp"];
+  int selectedindex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,6 +63,193 @@ class _homepageState extends State<homepage> {
                     ),
                   ),
                 ),
+              ),
+              SizedBox(height: 20),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 4),
+                    child: Text(
+                      "Categories",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Wrap(
+                    spacing: 10,
+                    children: List.generate(categories.length, (index) {
+                      return ChoiceChip(
+                        label: Text(categories[index]),
+                        selected: selectedindex == index,
+                        onSelected: (value) {
+                          setState(() {
+                            selectedindex = index;
+                          });
+                        },
+
+                        selectedColor: const Color(0xff0B5D4B),
+                        backgroundColor: Colors.white,
+
+                        labelStyle: TextStyle(
+                          color: selectedindex == index
+                              ? Colors.white
+                              : Colors.grey,
+                        ),
+
+                        shape: StadiumBorder(
+                          side: BorderSide(color: Colors.grey.shade300),
+                        ),
+                      );
+                    }),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 5),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Stack(
+                            children: [
+                              // Image
+                              Image.asset(
+                                "assets/images/image 4.jpg",
+                                height: 216,
+                                width: 173,
+                                fit: BoxFit.cover,
+                              ),
+
+                              // Gradient
+                              Positioned.fill(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                      begin: Alignment.topCenter,
+                                      end: Alignment.bottomCenter,
+                                      colors: [
+                                        const Color.fromARGB(104, 0, 0, 0),
+                                        const Color.fromARGB(
+                                          255,
+                                          46,
+                                          46,
+                                          46,
+                                        ).withOpacity(0.2),
+                                        Colors.black.withOpacity(0.9),
+                                      ],
+                                      stops: const [0.5, 0.75, 1.0],
+                                    ),
+                                  ),
+                                ),
+                              ),
+
+                              // Text
+                              Positioned(
+                                left: 15,
+                                right: 15,
+                                bottom: 15,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: const [
+                                    Text(
+                                      "Mountain",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    SizedBox(height: 4),
+                                    Text(
+                                      "Switzerland",
+                                      style: TextStyle(
+                                        color: Colors.white70,
+                                        fontSize: 13,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(width: 8),
+
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20),
+                        child: Stack(
+                          children: [
+                            // Image
+                            Image.asset(
+                              "assets/images/images 5.jpg",
+                              height: 216,
+                              width: 173,
+                              fit: BoxFit.cover,
+                            ),
+
+                            // Gradient
+                            Positioned.fill(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomCenter,
+                                    colors: [
+                                      const Color.fromARGB(104, 0, 0, 0),
+                                      const Color.fromARGB(
+                                        255,
+                                        46,
+                                        46,
+                                        46,
+                                      ).withOpacity(0.2),
+                                      Colors.black.withOpacity(0.9),
+                                    ],
+                                    stops: const [0.5, 0.75, 1.0],
+                                  ),
+                                ),
+                              ),
+                            ),
+
+                            // Text
+                            Positioned(
+                              left: 15,
+                              right: 15,
+                              bottom: 15,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisSize: MainAxisSize.min,
+                                children: const [
+                                  Text(
+                                    "Beach",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 4),
+                                  Text(
+                                    "Maldives",
+                                    style: TextStyle(
+                                      color: Colors.white70,
+                                      fontSize: 13,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
             ],
           ),
